fe4a524878dd4c69fa4050b911cd0f63950a04b8
brayanlrs28 <riverasantiagobrayan@gmail.com>
2023-04-05 13:35:28 -0500

    update clean properties

--- src/test/java/com/nttdata/btc/discoveryapp/NttdataBtcDiscoveryApplicationTests.java
+++ src/test/java/com/nttdata/btc/discoveryapp/NttdataBtcDiscoveryApplicationTests.java
@@ -3,8 +3,11 @@ package com.nttdata.btc.discoveryapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class NttdataBtcDiscoveryApplicationTests {
 
+	@Test
+	void contextLoads() {
+	}
 
 }
